d8c5cb2fb2acff54a8435d1c4d0a6291cd7449fb
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-30 21:46:09 +0900

    drm: exposure_adjustment: reduce usage of ifdef
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Change-Id: I04aced6835962b5075d024937cb3a0688235a82c
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -26,9 +26,7 @@
 
 #include <linux/double_click.h>
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 #include "exposure_adjustment.h"
-#endif
 
 /**
  * topology is currently defined by a set of following 3 values:
@@ -820,12 +818,10 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 {
 	int rc = 0;
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 		if (ea_panel_is_enabled()) {
 			ea_panel_mode_ctrl(panel, 0);
 			panel->resend_ea = true;
 		}
-#endif
 
 	if (panel->doze_enabled)
 		rc = dsi_panel_update_doze(panel);
@@ -836,12 +832,10 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 		pr_err("[%s] failed to send FOD HBM cmd, rc=%d\n",
 				panel->name, rc);
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
 		if (panel->resend_ea) {
 			ea_panel_mode_ctrl(panel, 1);
 			panel->resend_ea = false;
 		}
-#endif
 
 	return rc;
 }
@@ -857,10 +851,8 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 
 	pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
 
-#ifdef CONFIG_EXPOSURE_ADJUSTMENT
         if (bl_lvl > 0)
                 bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
-#endif
 
 	switch (bl->type) {
 	case DSI_BACKLIGHT_WLED:
